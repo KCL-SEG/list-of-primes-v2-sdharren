2ad9ebcc64245e663f9db9621b8dd1b0ee6b2ccd
sdharren <sdharren@yahoo.co.uk>
2022-12-07 23:11:33 +0000

    Update primes.py

--- primes.py
+++ primes.py
@@ -12,7 +12,7 @@ def primes(number_of_primes):
             list.append(i)
             if len(list) == number_of_primes:
                 break
-            for j in range(j*j, 1000 + 1, j):
+            for j in range(i*i, 1000 + 1, i):
                 set.add(j)
 
     return list[:number_of_primes]
